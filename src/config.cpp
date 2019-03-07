6e71aa03bdc85efe385b1e75f440350c1fd4c684
Richard Mak <richardwmak@gmail.com>
2019-03-07 09:06:26 +0100

    Reorganise config.cpp/hpp Add variables for clamping to config

--- src/config.cpp
+++ src/config.cpp
@@ -5,16 +5,21 @@ namespace Config
     double time_step = 0.4;
     float  visc      = 0.001;
     int    win_width, win_height;
-    bool   color_dir  = false;
-    float  vec_scale  = 1000;
-    bool   draw_smoke = false;
-    bool   draw_vecs  = true;
-    int    scalar_col = COLOR_BLACKWHITE;
-    bool   frozen     = false;
-    bool   gradient   = true;
-    int    num_verts  = 2;
-    bool   scaling    = false;
     float  num_glyphs = 100;
+
+    bool  color_dir  = false;
+    bool  draw_smoke = false;
+    bool  draw_vecs  = true;
+    bool  frozen     = false;
+    float vec_scale  = 1000;
+    bool  scaling    = false;
+    int   clamp_min  = 0;
+    int   clamp_max  = 1;
+
+    bool gradient  = true;
+    int  num_bands = 7;
+
+    int scalar_col = COLOR_BLACKWHITE;
 
     int scalar_to_draw = SCALAR_SMOKE;
     int vector_to_draw = VECTOR_VELOCITY;
--- src/config.hpp
+++ src/config.hpp
@@ -7,36 +7,35 @@ namespace Config
     const int     NUM_CELLS = GRID_SIZE * GRID_SIZE; // number of nodes we are dealing with
     extern double time_step;                         // simulation time step
     extern float  visc;                              // fluid viscosityx1
+    extern int    win_width, win_height;             // size of the graphics window, in pixels
+    extern float  num_glyphs;                        // how many glyphs to draw
 
     // VISUALIZATION PARAMETERS
-    extern int   win_width, win_height; // size of the graphics window, in pixels
-    extern bool  color_dir;             // use direction color-coding or not
-    extern float vec_scale;             // scaling of hedgehogs
-    extern bool  draw_smoke;            // draw the smoke or not
-    extern bool  draw_vecs;             // draw the vector field or not
-    extern float num_glyphs;            // how many glyphs to draw
-    const int   COLOR_BLACKWHITE = 0; // different types of color mapping: black-and-white, rainbow,
-    const int   COLOR_RAINBOW    = 1; // banded
-    const int   COLOR_RED_WHITE  = 2; //
-    extern int  scalar_col;           // method for scalar coloring
-    extern bool frozen;               // toggles on/off the animation
-    extern bool scaling;              // scaling if true, clamping if false;
+    extern bool  color_dir;  // use direction color-coding or not
+    extern bool  draw_smoke; // draw the smoke or not
+    extern bool  draw_vecs;  // draw the vector field or not
+    extern bool  frozen;     // toggles on/off the animation
+    extern float vec_scale;  // scaling of hedgehogs
+    extern bool  scaling;    // scaling if true, clamping if false;
+    extern int   clamp_min;  // clamping values if we are clamping
+    extern int   clamp_max;
+
+    extern bool gradient;  // draw a gradient if true, use banding if false
+    extern int  num_bands; // number of bands if gradient is false
+
+    // what color do we draw the scalar?
+    extern int scalar_col;           // method for scalar coloring
+    const int  COLOR_BLACKWHITE = 0; // different types of color mapping: black-and-white, rainbow,
+    const int  COLOR_RAINBOW    = 1; // banded
+    const int  COLOR_RED_WHITE  = 2; //
 
     // setting that determines what type of scalar/vector gets drawn
+    extern int scalar_to_draw;
+    extern int vector_to_draw;
+
     const int SCALAR_SMOKE    = 0;
     const int SCALAR_FORCE    = 1;
     const int SCALAR_VELOCITY = 2;
     const int VECTOR_VELOCITY = 0;
     const int VECTOR_FORCE    = 1;
-
-    extern int scalar_to_draw;
-    extern int vector_to_draw;
-
-    extern bool gradient;  // how to draw color_bar
-    extern int  num_verts; //
-
-    const int LOWER_LEFT  = 0;
-    const int LOWER_RIGHT = 1;
-    const int UPPER_RIGHT = 2;
-    const int UPPER_LEFT  = 3;
 } // namespace Config
